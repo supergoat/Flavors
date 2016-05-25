4f86012ebe281d71af9a5827cb77e74aa68a4cb1
super goat <p.nicolaou.13@gmail.com>
2016-05-25 20:25:55 +0100

    added new directive, to submit comments by pressing enter

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,2 @@
 // public/js/app.js
-angular.module('flavors', ['ui.router', 'appRoutes', 'MainCtrl', 'AuthCtrl', 'NavCtrl', 'ProfileCtrl', 'UserProfileCtrl', 'FindFriendsCtrl', 'FlavorsCtrl', 'fileUploadService', 'flavorsService', 'friendsService', 'currentUserService','authService']);
+angular.module('flavors', ['ui.router', 'appRoutes', 'MainCtrl', 'AuthCtrl', 'NavCtrl', 'ProfileCtrl', 'UserProfileCtrl', 'FindFriendsCtrl', 'FlavorsCtrl', 'fileUploadService', 'flavorsService', 'friendsService', 'currentUserService','authService', 'enterSubmit']);
